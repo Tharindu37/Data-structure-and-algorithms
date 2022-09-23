70f9f177ce2a46f8f50fdd924c62ce87df367853
Tharindu37 <tharindut520@gmail.com>
2022-09-23 16:46:03 +0530

    Inserting an item at the end of the list

---
+++ Linked Lists/LinkedLists.java
@@ -0,0 +1,50 @@
+class Lists{
+	private Node first;
+	
+	public void add(int data){
+		Node node = new Node(data);
+		if(first==null){
+			first = node;
+		}else{
+			Node temp = first;
+			while(temp.next != null){
+				temp = temp.next;
+			}
+			temp.next = node;
+		}
+	}
+	
+	public void printLists(){
+		if(first != null){
+			Node temp = first;
+			System.out.print("["+temp.data+", ");
+			while(temp.next != null){
+				temp = temp.next;
+				System.out.print(temp.data+", ");
+			}
+		}
+		System.out.print(first==null?"empty]":"\b\b]");
+	}
+	
+	class Node{
+		private int data;
+		Node next;
+		
+		Node(int data){
+			this.data=data;
+		}
+	}
+}
+
+class LinkedLists{
+	public static void main(String args[]){
+		Lists lists = new Lists();
+		lists.add(10);
+		lists.add(20);
+		lists.add(30);
+		lists.add(40);
+		lists.add(50);
+		
+		lists.printLists();
+	}
+}
